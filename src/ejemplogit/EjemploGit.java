7141d0d85f89bf13370f254211134e4dff4c396c
Esteban37 <estebandsc9937@hotmail.com>
2018-07-27 10:47:47 -0500

    V1.1
    Comentario corto

--- src/ejemplogit/EjemploGit.java
+++ src/ejemplogit/EjemploGit.java
@@ -17,6 +17,7 @@ public class EjemploGit {
     public static void main(String[] args) {
         // TODO code application logic here
         // Un pequeño cambio
+        // Otro pequeño cambio
     }
     
 }
